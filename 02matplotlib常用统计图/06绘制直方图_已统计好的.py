b9a7f0b2b7b2855176f263bdcd92bb43f00bddaf
MengxiaJin <13403465043@163.com>
2019-08-14 13:55:36 +0800

    import matplotlib.pyplot as plt plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签 plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
    interval = [0,5,10,15,20,25,30,35,40,45,60,90]
    width = [5,5,5,5,5,5,5,5,5,15,30,60]
    quantity = [836,2737,3723,3926,3596,1438,3273,642,824,613,215,47]
    
    plt.bar(range(12),quantity,width=1)
    
    #设置x轴的刻度
    _x = [i-0.5 for i in range(13)]
    _xtick_labels = interval+[150]
    plt.xticks(_x,_xtick_labels)
    
    plt.grid()
    plt.show()

---
+++ 02matplotlib常用统计图/06绘制直方图_已统计好的.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
+plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
+
+interval = [0,5,10,15,20,25,30,35,40,45,60,90]
+width = [5,5,5,5,5,5,5,5,5,15,30,60]
+quantity = [836,2737,3723,3926,3596,1438,3273,642,824,613,215,47]
+
+plt.bar(range(12),quantity,width=1)
+
+#设置x轴的刻度
+_x = [i-0.5 for i in range(13)]
+_xtick_labels = interval+[150]
+plt.xticks(_x,_xtick_labels)
+
+plt.grid()
+plt.show()
