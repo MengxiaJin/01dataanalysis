480734fb021d3566edd192b44e1293ce488e9b5e
MengxiaJin <48507495+MengxiaJin@users.noreply.github.com>
2019-08-20 09:57:21 +0800

    Create 03一万本书数据分析.py
    一万本书数据分析

---
+++ 04pandas学习/03一万本书数据分析.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
+plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
+data_file = "books.csv"
+data_books = pd.read_csv(data_file)
+data_books = data_books[pd.notnull(data_books["original_publication_year"])]
+data_year = data_books.groupby(by="original_publication_year")["title"].count()
+print (data_year)
+data_average_rating = data_books.groupby(by="original_publication_year")["average_rating"].mean()
+
+print (data_average_rating)
+
+_x = data_average_rating.index
+_y = data_average_rating.values
+
+plt.figure(figsize=(20,8),dpi=80)
+plt.plot(range(len(_x)),_y)
+plt.xticks(list(range(len(_x)))[::10],_x[::10])
+plt.show()
